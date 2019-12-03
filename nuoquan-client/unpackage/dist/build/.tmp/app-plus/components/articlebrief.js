5e46b1488584fb75bd71d88cf35321d1f93c3a1f
Zhirui <jerrio20@outlook.com>
2019-12-03 19:42:10 +0800

    after merge gutta

--- nuoquan-client/unpackage/dist/build/.tmp/app-plus/components/articlebrief.js
+++
@@ -1,1 +0,0 @@
-(global["webpackJsonp"]=global["webpackJsonp"]||[]).push([["components/articlebrief"],{"0d7e":function(e,o,n){"use strict";n("5ad9");var a=c(n("b0ce")),t=c(n("64d6"));function c(e){return e&&e.__esModule?e:{default:e}}Page((0,a.default)(t.default))}},[["0d7e","common/runtime","common/vendor"]]]);
