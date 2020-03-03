1b218d63373cd44013329a8567fcd84063694f60
Zhirui <jerrio20@outlook.com>
2020-03-03 13:37:07 +0800

    before guetta

--- nuoquan-client/main.js
+++ nuoquan-client/main.js
@@ -20,13 +20,16 @@ Vue.prototype.$store = store // 挂载 vueX
 // Vue.prototype.$serverUrl = "http://127.0.0.1:8080"
 // Vue.prototype.$wsServerUrl = "wss://127.0.0.1:8088/ws"
 
+Vue.prototype.$serverUrl = "http://192.168.124.8:8080"
+Vue.prototype.$wsServerUrl = "wss://192.168.124.8:8088/ws"
+
 // 服务器地址
 //Vue.prototype.$serverUrl = "http://129.28.130.27:8080/nottinghome"
 //Vue.prototype.$wsServerUrl = "ws://129.28.130.27:8088/ws"
 
 // 安全服务器地址
-Vue.prototype.$serverUrl = "https://www.checkchack.cn:8443/nottinghome"
-Vue.prototype.$wsServerUrl = "wss://www.checkchack.cn:8088/ws"
+// Vue.prototype.$serverUrl = "https://www.checkchack.cn:8443/nottinghome"
+// Vue.prototype.$wsServerUrl = "wss://www.checkchack.cn:8088/ws"
 
 /**
  * 获取当前用户信息（我）
