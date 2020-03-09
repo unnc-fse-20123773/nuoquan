c03ebd5bce4ba0cc1551478d7e2c6f9fb0089542
guetta <guetta.xia@outlook.com>
2020-03-09 22:35:00 +0800

    Add back to top in index and votepage

--- nuoquan-client/pages/tabPages/index.vue
+++ nuoquan-client/pages/tabPages/index.vue
@@ -61,7 +61,7 @@ export default {
 			userInfo: '',
 			totalPage: 1,
 			currentPage: 1,
-			scrollTop: 0,
+			scrollTop: -1,
 			old: {
 				scrollTop: 0
 			},
@@ -126,6 +126,9 @@ export default {
 
 	methods: {
 		onClickTab(e){
+			setTimeout(() => {
+				this.goTop()
+			}, 200)
 			//刷新
 			if(e.url == "/"+this.getCurrentPage().route){
 				this.showArticles(1);
@@ -304,6 +307,9 @@ export default {
 			this.$nextTick(function() {
 				this.scrollTop = 0;
 			});
+			setTimeout(() => {
+				this.scrollTop = -1;
+			}, 200)
 			uni.showToast({
 				icon: 'none',
 				title: '纵向滚动 scrollTop 值已被修改为 0'
