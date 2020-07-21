b2c7f3d5ee2e22d2d5ae58ab43c4924d5608cfd6
GuettaXia <46324076+GuettaXia@users.noreply.github.com>
2020-07-21 17:21:57 +0800

    Changed color of oneMessage in chatpage

--- nuoquan-client/pages/chatpage/oneMessage.vue
+++ nuoquan-client/pages/chatpage/oneMessage.vue
@@ -114,8 +114,8 @@
 	.send .content {
 		display: inline-flex;
 		flex-direction: column;
-		width: 428upx;
-		background: #FCCB2E;
+		max-width: 428upx;
+		background: rgba(254,240,231,1);
 		border-radius: 8pt 8pt 0pt 8pt;
 		margin-right: 80upx;
 		margin-bottom: 30upx;
@@ -129,8 +129,8 @@
 		height: 18px;
 		line-height: 18px;
 		max-width: 100px;
-		color: white;
 		margin-right: 6px;
+		color: #939393;
 	}
 
 	.status-send {
@@ -146,7 +146,6 @@
 	.contentText-send {
 		width: 100%;
 		font-size: 12px;
-		color: white;
 		word-break: break-all;
 		white-space: pre-line;
 	}
@@ -163,7 +162,7 @@
 	.recieve .content {
 		display: inline-flex;
 		flex-direction: column;
-		width: 428upx;
+		max-width: 428upx;
 		background: #FFF4DE;
 		border-radius: 0pt 8pt 8pt 8pt;
 		margin-top: 30upx;
@@ -185,7 +184,7 @@
 	}
 
 	.contentText-recieve {
-		width: 400upx;
+		width: 100%;
 		font-size: 12px;
 		word-break: break-all;
 		white-space: pre-line;
